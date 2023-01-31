32e6ed8ff35747add30bcaac7ad02c459aff6322
Alexis Davidson <alexisdavidson00@gmail.com>
2023-01-31 12:37:36 +0700

    d

--- lottery/src/lottery.rs
+++ lottery/src/lottery.rs
@@ -76,7 +76,7 @@ pub trait Lottery {
         require!(self.user_reward(&caller).is_empty(), "Already received reward");
 
         let current_block_timestamp = self.blockchain().get_block_timestamp();
-        let reward_index = current_block_timestamp % (self.rew_vec().len()) as u64 + 1_u64;
+        let reward_index = current_block_timestamp % (self.rew_vec().len()) as usize + 1_usize;
         let reward = self.rew_vec().get(reward_index);
         self.user_reward(&caller).set(&reward);
 
