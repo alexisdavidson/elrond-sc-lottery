19780f18a5499089878e881eedf4a3323dad5612
Alexis Davidson <alexisdavidson00@gmail.com>
2023-01-31 12:28:04 +0700

    b

--- lottery/src/lottery.rs
+++ lottery/src/lottery.rs
@@ -42,16 +42,16 @@ pub trait Lottery {
         self.accepted_payment_token_id().set(&token_id);
 
         for i in 0..rew_1 {
-            self.rew_vec().push(1);
+            self.rew_vec().push(&1);
         }
         for i in 0..rew_2 {
-            self.rew_vec().push(2);
+            self.rew_vec().push(&2);
         }
         for i in 0..rew_3 {
-            self.rew_vec().push(3);
+            self.rew_vec().push(&3);
         }
         for i in 0..rew_4 {
-            self.rew_vec().push(4);
+            self.rew_vec().push(&4);
         }
     }
 
